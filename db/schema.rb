c8ce7eff85c370bf1210493412665d71ad39d62c
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-07-07 18:44:56 +0300

    Use password_digest column

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_07_145755) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
