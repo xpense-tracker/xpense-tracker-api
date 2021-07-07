29249355be1d0e159d93ded6ecf56fef4761919b
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-07-07 19:20:13 +0300

    json api for users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 module Api
+  # Register users
   class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
 
       if @user.save
-        render json: @user, status: :created, location: @user
+        # TODO: replace with real location
+        render json: @user, status: :created, location: nil
       else
-        render json: @user.errors, status: :unprocessable_entity
+        render json: @user, status: :unprocessable_entity
       end
     end
 
