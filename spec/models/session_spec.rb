51998217aa968421dad640322ae0de3dd629a430
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-07-11 01:20:00 +0300

    Add contexts to test

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -3,4 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Session do
+  subject(:session) { described_class.new({}) }
+
+  describe '#valid?' do
+    context 'without email' do
+    end
+
+    context 'without password' do
+    end
+
+    context 'with wrong password' do
+    end
+
+    context 'with correct credentials' do
+    end
+  end
+
+  describe '#token' do
+  end
 end
