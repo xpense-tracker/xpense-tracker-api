056845b719a52e13cc69e90568d75998d68d23f9
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-07-11 15:51:33 +0300

    Add test for session validation

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
   gem 'byebug'
+  gem 'factory_bot_rails'
+  gem 'faker'
   gem 'rspec-rails', '~> 5.0.0'
 end
 
--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -2,20 +2,36 @@
 
 require 'rails_helper'
 
+require 'support/factory_bot'
+
 RSpec.describe Session do
-  subject(:session) { described_class.new({}) }
+  subject(:session) { described_class.new(params) }
 
   describe '#valid?' do
     context 'without email' do
+      let(:params) { { password: '123456' } }
+
+      it { is_expected.to be_invalid }
     end
 
     context 'without password' do
+      let(:params) { { email: 'user@example.org' } }
+
+      it { is_expected.to be_invalid }
     end
 
     context 'with wrong password' do
+      let(:params) { { email: 'user@example.org', password: '123456' } }
+
+      it { is_expected.to be_invalid }
     end
 
     context 'with correct credentials' do
+      before { create(:user, params) }
+
+      let(:params) { { email: 'user@example.org', password: '123456' } }
+
+      it { is_expected.to be_valid }
     end
   end
 
