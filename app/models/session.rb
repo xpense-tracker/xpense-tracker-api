62989408558a6a21ef347063cc401af7524bc306
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-07-11 17:12:19 +0300

    Add spec for session

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,17 +5,18 @@ class Session
   include ActiveModel::Validations
   include ActiveModel::Serialization
 
-  def initialize(session_params)
+  def initialize(session_params, jwt_codec = JwtCodec.new)
     @email = session_params[:email]
     @password = session_params[:password]
+    @jwt_codec = jwt_codec
   end
 
   validates :email, presence: true
   validates :password, presence: true
-  validate :authenticated
+  validate :user_must_be_authenticated
 
   def token
-    JwtCodec.new.encode({ id: user.id })
+    @jwt_codec.encode({ id: user.id })
   end
 
   private
@@ -26,7 +27,7 @@ class Session
     @user ||= User.find_by(email: @email)&.authenticate(@password)
   end
 
-  def authenticated
+  def user_must_be_authenticated
     errors.add :base, :authenticated if user.blank?
   end
 end
--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -27,16 +27,26 @@ RSpec.describe Session do
     end
 
     context 'with correct credentials' do
-      before { create(:user, params) }
-
       let(:params) { { email: 'user@example.org', password: '123456' } }
+
+      before { create(:user, params) }
 
       it { is_expected.to be_valid }
     end
   end
 
   describe '#token' do
-    # TODO: add specs for Session#token
-    pending 'Session#token'
+    subject(:session) { described_class.new(params, fake_jwt_codec) }
+
+    let(:params) { { email: 'user@example.org', password: '123456' } }
+    let(:fake_jwt_codec) { instance_double('JwtCodec', encode: 'ey1.2.3') }
+
+    let(:user) { create(:user, params) }
+
+    it 'finds user' do
+      expect(fake_jwt_codec).to receive(:encode).with({ id: user.id })
+
+      expect(session.token).to eq('ey1.2.3')
+    end
   end
 end
