84b262530dfb59dd065add667d3204f0b7c40dcc
Dmitry Barskov <dmitry.barskov@flatstack.com>
2021-08-11 11:39:27 +0300

    Update config/routes.rb
    Co-authored-by: EgorBarakhov <egor.barakhov@flatstack.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :sessions
+      resources :sessions, only: %i[create]
     end
   end
 end
