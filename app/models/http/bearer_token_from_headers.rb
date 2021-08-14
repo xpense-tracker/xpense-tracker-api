4bfc91f52aa2b3682fb13145cd59bd913dd55d76
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-08-14 23:09:09 +0300

    Add missing specs

--- app/models/http/bearer_token_from_headers.rb
+++ app/models/http/bearer_token_from_headers.rb
@@ -3,14 +3,20 @@
 module Http
   # Extracts authentication token from Authorization HTTP header.
   class BearerTokenFromHeaders < ApplicationModel
+    class MissingToken < RuntimeError; end
+
+    class UnsupportedType < RuntimeError; end
+
     def initialize(headers)
       super()
       @headers = headers
     end
 
     def to_s
-      type, token = @headers['Authorization']&.split
-      raise unless type == 'Bearer'
+      raise MissingToken unless @headers['Authorization']
+
+      type, token = @headers['Authorization'].split
+      raise UnsupportedType unless type == 'Bearer'
 
       token
     end
---
+++ spec/models/authentication/user_by_id_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+require 'support/factory_bot'
+
+RSpec.describe Authentication::UserById do
+  let(:user) { described_class.new(id) }
+
+  describe '#to_model' do
+    context 'when user with given id exists' do
+      let!(:user_from_database) { create(:user) }
+      let(:id) { user_from_database.id }
+
+      it 'returns user from database' do
+        expect(user.to_model).to eq(user_from_database)
+      end
+    end
+
+    context 'when there is no user with such id' do
+      let(:id) { 'some-id' }
+
+      it 'raises error' do
+        expect { user.to_model }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+end
