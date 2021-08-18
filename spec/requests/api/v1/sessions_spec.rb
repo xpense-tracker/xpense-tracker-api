7bb38acbcc4f002e541a0049da90b6c2c94dd9fb
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-08-18 09:45:31 +0300

    Refactor session spec

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -6,8 +6,9 @@ require 'support/matchers/have_json_body'
 
 RSpec.describe 'Api::V1::Sessions' do
   describe 'POST /api/v1/sessions' do
-    subject(:send_request) do
+    subject(:http_response) do
       post api_v1_sessions_path, params: { session: session_params }
+      response
     end
 
     let(:session_params) { { email: 'user@example.org', password: '123456' } }
@@ -15,21 +16,13 @@ RSpec.describe 'Api::V1::Sessions' do
     context 'when account with given credentials exists' do
       before { create(:user, session_params) }
 
-      it 'return a token' do
-        send_request
-
-        expect(response).to have_http_status(:ok)
-          .and have_json_body(session: have_key(:token))
-      end
+      it { is_expected.to have_http_status(:ok) }
+      it { is_expected.to have_json_body(session: have_key(:token)) }
     end
 
     context 'when email or password are wrong' do
-      it 'return a successful response' do
-        send_request
-
-        expect(response).to have_http_status(:unauthorized)
-          .and have_json_body(session: have_key(:errors))
-      end
+      it { is_expected.to have_http_status(:unauthorized) }
+      it { is_expected.to have_json_body(session: have_key(:errors)) }
     end
   end
 end
