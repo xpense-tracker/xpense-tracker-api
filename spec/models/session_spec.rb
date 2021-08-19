4e3ed87f71f4025eb628db44feb64fbb84106812
Dmitry Barskov <dmitry.barskov@flatstack.com>
2021-08-19 21:23:34 +0300

    RSpec GitHub CI (#8)
    * Create users resource
    
    * Update schema
    
    * Add serializer
    
    * Use password_digest column
    
    * Add unique index for users.email
    
    * Add validations to user
    
    * json api for users
    
    * Use user local variable
    
    * Add route to sessions
    
    * Controller for authentication
    
    * session model
    
    * add jwt
    
    * Use 80 as line length
    
    * Add rspec
    
    * Install rspec
    
    * Address code style offences
    
    * Address code style offences
    
    * Add contexts to test
    
    * Add test for session validation
    
    * Address code style offences
    
    * Add RSpec to GitHub Actions
    
    * Fix
    
    * Add .env.example file
    
    * Add dotenv
    
    * Add checkout action
    
    * Do not use jobs
    
    * fix
    
    * add postgres service
    
    * Add spec for session
    
    * Create v1 namespace in API
    
    * Use UUID as IDs
    
    * Use JSON adapter for AMS
    
    * separate linter rules for specs
    
    * Update .env.example
    
    * Address the review
    
    Co-authored-by: Egor Barakhov <egor.barakhov@flatstack.com>
    
    Co-authored-by: Egor Barakhov <egor.barakhov@flatstack.com>

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -27,16 +27,23 @@ RSpec.describe Session do
     end
 
     context 'with correct credentials' do
-      before { create(:user, params) }
-
       let(:params) { { email: 'user@example.org', password: '123456' } }
+
+      before { create(:user, params) }
 
       it { is_expected.to be_valid }
     end
   end
 
   describe '#token' do
-    # TODO: add specs for Session#token
-    pending 'Session#token'
+    subject(:token) { session.token }
+
+    let(:session) { described_class.new(params, fake_jwt_codec) }
+    let(:params) { { email: 'user@example.org', password: '123456' } }
+    let(:fake_jwt_codec) { instance_double('JwtCodec', encode: 'ey1.2.3') }
+
+    before { create(:user, params) }
+
+    it { is_expected.to eq('ey1.2.3') }
   end
 end
