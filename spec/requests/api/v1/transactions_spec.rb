eba00d8ab3a9dbcc8f246842885129e3a01631e0
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-08-21 19:31:41 +0300

    Add spec subject and master

--- spec/requests/api/v1/transactions_spec.rb
+++ spec/requests/api/v1/transactions_spec.rb
@@ -4,6 +4,11 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Transactions' do
   describe 'GET /index' do
+    subject(:http_response) do
+      post api_v1_transactions_path
+      response
+    end
+
     pending "add some examples (or delete) #{__FILE__}"
   end
 end
