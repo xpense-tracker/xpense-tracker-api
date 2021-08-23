cf883b90d0905beba04b54e1f0388aff9cde6bab
Dmitry Barskov <dmitry.barskov@flatstack.com>
2021-08-23 10:18:18 +0300

    Add specs for models (#11)
    * Create users resource
    
    * Update schema
    
    * Add serializer
    
    * Use password_digest column
    
    * Add unique index for users.email
    
    * Add validations to user
    
    * json api for users
    
    * Use user local variable
    
    * Add route to sessions
    
    * Controller for authentication
    
    * session model
    
    * add jwt
    
    * Use 80 as line length
    
    * Add rspec
    
    * Install rspec
    
    * Address code style offences
    
    * Address code style offences
    
    * Add contexts to test
    
    * Add test for session validation
    
    * Address code style offences
    
    * Add RSpec to GitHub Actions
    
    * Fix
    
    * Add .env.example file
    
    * Add dotenv
    
    * Add checkout action
    
    * Do not use jobs
    
    * fix
    
    * add postgres service
    
    * Add spec for session
    
    * Refactoring
    
    * Add test for jwt codec
    
    * Add specs for models
    
    * Create v1 namespace in API
    
    * Use UUID as IDs
    
    * Use JSON adapter for AMS
    
    * separate linter rules for specs
    
    * Move logic to Validator
    
    Co-authored-by: Egor Barakhov <egor.barakhov@flatstack.com>
    
    * add spec for the validator
    
    * Address code style offences
    
    * Revert changes in .rubocop.yml
    
    * update test
    
    * revert changes
    
    Co-authored-by: Egor Barakhov <egor.barakhov@flatstack.com>

--- app/models/authenticated_user.rb
+++ app/models/authenticated_user.rb
@@ -3,14 +3,14 @@
 # Authenticates a user and provides their ID
 class AuthenticatedUser < ApplicationModel
   def initialize(credentials)
-    super
+    super()
     @email = credentials[:email]
     @password = credentials[:password]
   end
 
   delegate :id, to: :user
 
-  attr_accessor :email, :password
+  attr_reader :email, :password
 
   validates :email, presence: true
   validates :password, presence: true
