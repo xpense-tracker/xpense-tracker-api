dc0b14e7eab93344d7e9cd5fa79bf0e0b8f9aff9
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-08-25 15:44:02 +0300

    add spec for update action

---
+++ spec/support/shared_contexts/when_user_signed_in.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Provides current user and authorization headers to all request methods
+RSpec.shared_context 'when user signed in' do
+  let(:current_user) { create(:user) }
+  let(:jwt) do
+    Authentication::Session.new(
+      current_user, Authentication::JwtCodec.new
+    ).access_token
+  end
+
+  %i(get post patch put head delete).each do |method_name|
+    define_method(method_name) do |path, *args, **kwargs|
+      super(
+        path, *args,
+        **kwargs.deep_merge(headers: { 'Authorization' => "Bearer #{jwt}" })
+      )
+    end
+  end
+end
