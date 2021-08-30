63b124f2bc7a35e8c9b5c8e4fd904ece30d2b6ac
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-08-30 15:10:13 +0300

    Address code style offences

--- db/migrate/20210829201423_change_users_email_type_to_citext.rb
+++ db/migrate/20210829201423_change_users_email_type_to_citext.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Change users.email type from text to citext to ignore case
 class ChangeUsersEmailTypeToCitext < ActiveRecord::Migration[6.1]
   def up
     change_column :users, :email, :citext
