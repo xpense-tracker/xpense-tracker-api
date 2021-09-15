f782ee2dcf003068ce99c3b7e30638d0ff625788
Dmitry Barskov <dmitry.barskov@flatstack.com>
2021-09-15 22:25:34 +0300

    📝 Add request spec

--- spec/requests/api/v1/categories_spec.rb
+++ spec/requests/api/v1/categories_spec.rb
@@ -1,7 +1,27 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe "Api::V1::Categories", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+require 'rails_helper'
+require 'support/factory_bot'
+
+require 'support/shared_contexts/when_user_signed_in'
+require 'support/matchers/have_json_body'
+
+RSpec.describe 'Api::V1::Categories' do
+  describe 'GET /api/v1/categories' do
+    subject(:http_response) do
+      get api_v1_categories_path
+      response
+    end
+
+    before do
+      create(:category, title: 'Public category')
+      create(:category, :private, title: 'Custom category', user: current_user)
+      create(:category, :private, title: 'Private category')
+    end
+
+    include_context 'when user signed in'
+
+    it { is_expected.to have_http_status(:ok) }
+    it { is_expected.to have_json_body(categories: be_an(Array)) }
   end
 end
