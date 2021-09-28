35d6e627d9bd0c3cd21b147bf669b92231f892db
Dmitry Barskov <dmitry.barskov@flatstack.com>
2021-09-28 23:37:25 +0300

    Predefine categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,51 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed
-# to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command
-# (or created alongside the database with db:setup).
-#
-# Examples:
-#
-# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-# Character.create(name: 'Luke', movie: movies.first)
+
+# TODO: Add icon URLs and description to all categories.
+# https://fonts.google.com/icons
+Category.insert_all!([
+  {
+    title: 'Fast food',
+    description: 'Quick service restaurants such as McDonald\'s',
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/fastfood/v12/24px.svg'
+  },
+  {
+    title: 'Groceries',
+    description: 'The food or goods in a supermarket',
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/shopping_cart/v17/24px.svg'
+  },
+  {
+    title: 'Transport',
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/commute/v12/24px.svg'
+  },
+  {
+    title: 'Entertainment',
+  },
+  {
+    title: 'Car',
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/directions_car/v17/24px.svg'
+  },
+  {
+    title: 'Clothes',
+  },
+  {
+    title: 'Gifts',
+  },
+  {
+    title: 'Communications',
+  },
+  {
+    title: 'Pets',
+  },
+  {
+    title: 'Health',
+  },
+  {
+    title: 'Toiletry',
+  },
+  {
+    title: 'Taxi',
+  },
+  {
+    title: 'Sports',
+  },
+])
