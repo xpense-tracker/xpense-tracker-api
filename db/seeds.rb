9156d8e99e6b58e88dd8ca1e36c74a8581853567
Dmitry Barskov <dmitry.barskov@flatstack.com>
2022-02-14 14:00:11 +0300

    Add icons

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-# TODO: Add icon URLs and description to all categories.
-# https://fonts.google.com/icons
-Category.insert_all!([
+# Icons from https://fonts.google.com/icons
+[
   {
     title: 'Fast food',
     description: 'Quick service restaurants such as McDonald\'s',
@@ -15,37 +14,52 @@ Category.insert_all!([
   },
   {
     title: 'Transport',
+    # TODO: Add description
     icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/commute/v12/24px.svg'
   },
   {
     title: 'Entertainment',
+    # TODO: Add description
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/live_tv/v11/24px.svg'
   },
   {
     title: 'Car',
+    description: 'Gas, repair service',
     icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/directions_car/v17/24px.svg'
   },
   {
     title: 'Clothes',
+    # TODO: Add description
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/health_and_safety/v7/24px.svg'
   },
   {
     title: 'Gifts',
-  },
-  {
-    title: 'Communications',
+    # TODO: Add description
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/card_giftcard/v14/24px.svg'
   },
   {
     title: 'Pets',
+    # TODO: Add description
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/pets/v12/24px.svg'
   },
   {
     title: 'Health',
+    # TODO: Add description
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/health_and_safety/v7/24px.svg'
   },
   {
     title: 'Toiletry',
+    # TODO: Add description
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/bathtub/v11/24px.svg'
   },
   {
     title: 'Taxi',
+    # TODO: Add description
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/local_taxi/v12/24px.svg'
   },
   {
     title: 'Sports',
-  },
-])
+    description: 'Sports equipment, clothes, nutrition; gym membership',
+    icon_url: 'https://fonts.gstatic.com/s/i/materialiconsoutlined/sports_soccer/v11/24px.svg'
+  }
+].each { |attrs| Category.create!(attrs) }
